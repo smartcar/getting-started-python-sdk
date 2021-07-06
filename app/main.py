6360006b4dbdb30152dde55598886bc03d480ff1
Nathan Bryant <nathan.bryant@smartcar.com>
2021-07-06 09:38:57 -0700

    fix: /vehicle
    Ran SDK revamp test plan with local `smartcar` package. Updated method
    name in '/vehicle' route to use `smartcar.get_vehicles()`. Updated
    `exchange` route to redirect to `/vehicle`.

--- app/main.py
+++ app/main.py
@@ -31,6 +31,11 @@ def login():
 
 @app.route("/exchange", methods=["GET"])
 def exchange_code():
+    """
+    To work, this route must be in your Smartcar developer dashboard as a Redirect URI.
+
+    i.e. "http://localhost:8000/exchange"
+    """
     code = request.args.get("code")
 
     # access our global variable and store our access tokens
@@ -39,7 +44,7 @@ def exchange_code():
     # in a production app you'll want to store this in some kind of
     # persistent storage
     access = client.exchange_code(code)
-    return "", 200
+    return redirect("/vehicle")
 
 
 @app.route("/vehicle", methods=["GET"])
@@ -48,7 +53,7 @@ def get_vehicle():
     global access
 
     # receive a `Vehicles` NamedTuple, which has an attribute of 'vehicles' and 'meta'
-    result = smartcar.get_vehicle_ids(access.access_token)
+    result = smartcar.get_vehicles(access.access_token)
 
     # get the first vehicle
     id_of_first_vehicle = result.vehicles[0]
